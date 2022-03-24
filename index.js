6169762ac256740f2bc08492314e99c47561a79a
Ajeet kumar <ajeet.gupta51@gmail.com>
2022-03-24 08:58:50 +0530

    commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const url = require('url');
+const critical = require('critical');
+const PORT = 8080;
+const app = express();
+app.listen(
+	PORT,
+	() => console.log(`server running on ${PORT}`)
+	);
+app.get('/criticalcss',(req,res) => {
+	const queryObj = url.parse(req.url, true).query;
+	//res.send(queryObj.url);
+	let criticalCss = ''; 
+	const {css, html, uncritical} = critical.generate({
+	  base: 'test/',
+	  src: 'index.html',
+	  width: 1300,
+	  height: 900,
+	});
+
+	criticalCss = critical.generate({
+	    base: 'test/',
+	    src: 'index.html',
+	    width: 1300,
+	    height: 900
+	}).then( ({css, html, uncritical}) => {
+		//return css;
+		res.json({"status": 200, critical_css: css});
+	    // criticalCss = css;
+	});
+	// res.send('critical css is done'+criticalCss)
+});
